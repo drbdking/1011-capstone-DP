1b756023db0aa51f874be48b1ed038f43268de13
dc5116 <dc5116@nyu.edu>
2023-11-30 11:42:21 -0500

    Debug

--- models.py
+++ models.py
@@ -29,7 +29,7 @@ class BinaryClassificationHead(nn.Module):
     Args:
         nn (_type_): _description_
     """
-    def __init__(self, input_size) -> None:
+    def __init__(self, input_size):
         super().__init__()
         self.dense = nn.Linear(input_size, input_size)
         self.dropout = nn.Dropout(0.5)
@@ -52,12 +52,12 @@ class AdversarialDecoder(nn.Module):
     def __init__(self, d_model=768, nhead=12, num_decoder_layers=6, dim_feedforward=2048,
                  tgt_vocab_size=10000,
                  dropout=0.1, activation=F.relu, layer_norm_eps=1e-5,
-                 batch_first=True, norm_first=False, bias=True, device=None, dtype=None) -> None:
+                 batch_first=True, norm_first=False, bias=True, device=None, dtype=None):
         super(AdversarialDecoder, self).__init__()
         # All you need from Bert is the vocab_size
-        decoder_layer = nn.TransformerDecoderLayer(d_model, nhead, dim_feedforward, dropout,
-                                                    activation, layer_norm_eps, batch_first,
-                                                    norm_first, bias)
+        decoder_layer = nn.TransformerDecoderLayer(d_model, nhead, dim_feedforward, dropout, 
+                                                   activation, layer_norm_eps, batch_first, 
+                                                   norm_first, bias)
         decoder_norm = nn.LayerNorm(d_model, eps=layer_norm_eps, bias=bias)
         self.decoder = nn.TransformerDecoder(decoder_layer, num_decoder_layers, decoder_norm)
         self.generator = nn.Linear(d_model, tgt_vocab_size)
