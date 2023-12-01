0a232f07e48d41c260eca2f7d2c666c50dd6fff2
dc5116 <dc5116@nyu.edu>
2023-11-30 23:03:32 -0500

    Show adv output

--- adv_ft.py
+++ adv_ft.py
@@ -26,6 +26,7 @@ def train_adv(train_loader, val_loader, adv_dict, embedding_dict, device, args):
         # train adv, get embedding (no grad), hidden state and label (input token ids)
         print("-" * 10)
         print(f"epoch {epoch + 1}/{args.num_epochs}")
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
         
         step = 0
         train_loss = 0
@@ -72,6 +73,14 @@ def train_adv(train_loader, val_loader, adv_dict, embedding_dict, device, args):
                     loss = adv_dict['loss_function'](output, input_ids)
                     val_loss += loss.item()
                     val_progress_bar.update(1)
+                    
+                # Decode the last batch
+                output_ids = torch.argmax(output, dim=1).detach().cpu()
+                original_seq = [x + " " + y for x, y in zip(batch['question1'], batch['question2'])]
+                pred_seq = tokenizer.batch_decode(output_ids)
+                for ref, pred in zip(original_seq, pred_seq):
+                    print(f"Original input: {ref} \nAdv output: {pred}")
+
             val_loss /= step
             print(f"epoch {epoch + 1} average val loss: {val_loss:.4f}")
             val_progress_bar.close()
