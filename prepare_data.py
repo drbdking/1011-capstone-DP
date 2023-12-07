e6ea034d5f83a4429431e26953690a70865cddb2
dc5116 <dc5116@nyu.edu>
2023-12-06 21:26:39 -0500

    Fix prepare data

--- prepare_data.py
+++ prepare_data.py
@@ -6,7 +6,7 @@ from data import *
 if __name__ == '__main__':
     df_qqp = pd.read_csv("data/quora_duplicate_questions.tsv", sep = '\t')
     df_embedding_train, df_threat_train, df_embedding_test, df_threat_test = split_data(df_qqp, ratios=[0.6, 0.2, 0.1, 0.1])
-    df_embedding_train.to_csv('data/qqp_embedding_train.tsv', sep='\t', index=False, header=False)
-    df_threat_train.to_csv('data/qqp_threat_train.tsv', sep='\t', index=False, header=False)
-    df_embedding_test.to_csv('data/qqp_embedding_test.tsv', sep='\t', index=False, header=False)
-    df_threat_test.to_csv('data/qqp_threat_test.tsv', sep='\t', index=False, header=False)
+    df_embedding_train.to_csv('data/qqp_embedding_train.tsv', sep='\t', index=False)
+    df_threat_train.to_csv('data/qqp_threat_train.tsv', sep='\t', index=False)
+    df_embedding_test.to_csv('data/qqp_embedding_test.tsv', sep='\t', index=False)
+    df_threat_test.to_csv('data/qqp_threat_test.tsv', sep='\t', index=False)
