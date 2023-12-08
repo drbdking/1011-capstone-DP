5fbadf057e4b73326e5ad7408daa003cec970b96
dc5116 <dc5116@nyu.edu>
2023-12-07 23:22:14 -0500

    ft

--- scripts/adv_fine_tune.sh
+++ scripts/adv_fine_tune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for alpha in 0.5 0.8 
+for alpha in 1 1.5 
 do
     echo "-------------------- fine-tuning, alpha = $alpha ---------------------";
-    python adv_ft.py --downsample 0.25 --adv_mode 0 --num_epochs 5 --alpha $alpha
+    python adv_ft.py --downsample 0.25 --adv_mode 0 --num_epochs 5 --alpha $alpha --batch_size 64
 done
