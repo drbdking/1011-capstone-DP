8462ca6094332a7e3bc49198e86bcff5e2a9b994
dc5116 <dc5116@nyu.edu>
2023-12-07 23:25:10 -0500

    script

--- scripts/adv_ft_langone.sh
+++ scripts/adv_ft_langone.sh
@@ -2,5 +2,5 @@
 for alpha in 1 1.5 
 do
     echo "-------------------- fine-tuning, alpha = $alpha ---------------------";
-    python adv_ft.py --downsample 0.25 --adv_mode 0 --num_epochs 5 --alpha $alpha
+    python adv_ft.py --downsample 0.25 --adv_mode 0 --num_epochs 5 --alpha $alpha --batch_size 64
 done
