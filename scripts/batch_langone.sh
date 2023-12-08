c4913c0d073c016bae5399d0c578d82cdc1cd83d
dc5116 <dc5116@nyu.edu>
2023-12-08 10:30:56 -0500

    Disable adv

--- scripts/batch_langone.sh
+++ scripts/batch_langone.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-for alpha in 0.5 0.8 1 
-do
-    for lr in 0.00001 0.000005
-        do
+alpha=0
+for lr in 0.00001 0.000005
+    do
         sbatch scripts/adv_fine_tune_langone.slurm $alpha $lr;
         echo "Submitted job for alpha = $alpha, lr = $lr"
-        done
-done
+    done
