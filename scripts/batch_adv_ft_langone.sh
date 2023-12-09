454e550fbb5f09ce5cb9f4942e3f19e9a5b50caa
dc5116 <dc5116@nyu.edu>
2023-12-09 00:41:18 -0500

    batch size

--- scripts/batch_adv_ft_langone.sh
+++ scripts/batch_adv_ft_langone.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 for alpha in 0.8 1.0 1.5 2.0
+do
+    for adv_lr in 5e-4 1e-5
     do
-        for adv_lr in 5e-4 1e-5
-            do
-                for embedding_mode in use_separate_embedding use_bert_embedding
-                    do
-                        sbatch scripts/adv_fine_tune_langone.slurm $alpha $adv_lr $embedding_mode;
-                        echo "Submitted job for alpha = $alpha, adv_lr = $adv_lr, $embedding_mode"
-                    done
-            done
+        for embedding_mode in use_separate_embedding use_bert_embedding
+        do
+            sbatch scripts/adv_fine_tune_langone.slurm $alpha $adv_lr $embedding_mode;
+            echo "Submitted job for alpha = $alpha, adv_lr = $adv_lr, $embedding_mode"
         done
     done
+done
